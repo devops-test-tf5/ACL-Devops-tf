602c1c61d9c6c154cfa05e8d44f4c2947ffe621f
devops-test-tf5 <105194113+devops-test-tf5@users.noreply.github.com>
2022-05-11 12:58:57 +0530

    initial commit with project related files

---
+++ src/main/java/com/acldigital/unaito/UnaitoApplication.java
@@ -0,0 +1,13 @@
+package com.acldigital.unaito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnaitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnaitoApplication.class, args);
+	}
+
+}
